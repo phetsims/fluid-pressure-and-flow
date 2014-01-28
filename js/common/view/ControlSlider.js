0cf0da9e92760cdef75c9b5f99e16457931c375e
jbphet <john.blanco@colorado.edu>
2014-01-28 14:21:53 -0800

    Fixed a silly syntax mistake.

--- js/common/view/ControlSlider.js
+++ js/common/view/ControlSlider.js
@@ -179,8 +179,8 @@ define( function( require ) {
       {
         title: options.title,
         fill: options.fill,
-        font: new PhetFont( { size: 19 } );
-    initiallyOpen: true,
+        font: new PhetFont( { size: 19 } ),
+        initiallyOpen: true,
         minWidth: 250,
         contentPosition: 'center',
         titlePosition: 'left',
