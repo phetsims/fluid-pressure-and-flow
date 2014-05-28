7906285ed4a357f63e3c715039e602b93a0bb816
Sam Reid <reids@colorado.edu>
2014-05-28 10:58:45 -0600

    Added mockup from the Java sim

--- js/watertower/view/WaterTowerScreenView.js
+++ js/watertower/view/WaterTowerScreenView.js
@@ -11,6 +11,10 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var ScreenView = require( 'JOIST/ScreenView' );
+  var Image = require( 'SCENERY/nodes/Image' );
+
+  // images
+  var mockupImage = require( 'image!FLUID_PRESSURE_AND_FLOW/water-tower-mockup.png' );
 
   /**
    * @param {WaterTowerModel} model
@@ -19,6 +23,19 @@ define( function( require ) {
   function WaterTowerScreenView( model ) {
     var thisView = this;
     ScreenView.call( thisView );
+
+    // Add a mock-up from the Java version.
+    //
+    // Please note that this is to help line up elements in the play area, and some user interface components from the Sun repo will
+    // be much bigger to make the sims usable on tablets.
+    //
+    // Also note, the sky and ground should extend to the sides of the browser window.  Please use OutsideBackgroundNode for this.
+    this.addChild( new Image( mockupImage, {
+      centerX: ScreenView.DEFAULT_LAYOUT_BOUNDS.centerX,
+      centerY: ScreenView.DEFAULT_LAYOUT_BOUNDS.centerY,
+      scale: ScreenView.DEFAULT_LAYOUT_BOUNDS.height / mockupImage.height,
+      opacity: 0.5
+    } ) );
   }
 
   return inherit( ScreenView, WaterTowerScreenView );
