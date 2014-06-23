92ac7878e696c59a9ba7336cf8db41579f1cf066
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-06-23 12:59:17 +0530

    Adding a contants module to hold all the constants related to this sim.

---
+++ js/watertower/Constants.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2002 - 2014. University of Colorado Boulder
+
+/**
+ * Constants used in this Sim
+ * @author Siddhartha Chinthapally (Actual Concepts) on 6/23/2014.
+ */
+define( function() {
+  'use strict';
+
+  return {
+    EARTH_GRAVITY: 9.8
+  };
+} );
