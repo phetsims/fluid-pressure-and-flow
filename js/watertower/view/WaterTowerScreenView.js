92c2a47b8b0a073fb275c7fb8acc5d0357923fd1
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-06-25 17:06:38 +0530

    Reset the water tower position to initial state on reset

--- js/watertower/view/WaterTowerScreenView.js
+++ js/watertower/view/WaterTowerScreenView.js
@@ -67,7 +67,8 @@ define( function( require ) {
 
     this.addChild( new OutsideBackgroundNode( this.layoutBounds.centerX, this.layoutBounds.centerY + 100, this.layoutBounds.width * 3, this.layoutBounds.height, this.layoutBounds.height ) );
 
-    this.addChild( new WaterTowerView( model.waterTower, { x: 0, y: 100} ) );
+    var waterTowerView = new WaterTowerView( model.waterTower, { x: 0, y: 100} );
+    this.addChild( waterTowerView );
 
     this.addChild( new FaucetNode( 1, model.faucetFlowRateProperty, model.isFaucetEnabledProperty, {
       horizontalPipeLength: 1000,
@@ -110,6 +111,7 @@ define( function( require ) {
     this.addChild( new ResetAllButton( {
       listener: function() {
         model.reset();
+        waterTowerView.y = 100;
       },
       scale: 0.66, x: 705, y: 450
     } ) );
