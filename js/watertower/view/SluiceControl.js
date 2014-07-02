b1cc5d4b295bcf316166d407d9610b1b2ef3f960
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-07-02 12:45:54 +0530

    Sluice control node as per the mock

--- js/watertower/view/SluiceControl.js
+++ js/watertower/view/SluiceControl.js
@@ -9,9 +9,17 @@ define( function( require ) {
 
     // modules
     var inherit = require( 'PHET_CORE/inherit' );
+    var Path = require( 'SCENERY/nodes/Path' );
+    var Shape = require( 'KITE/Shape' );
     var Node = require( 'SCENERY/nodes/Node' );
+    var Rectangle = require( 'SCENERY/nodes/Rectangle' );
     var ABSwitch = require( 'SUN/ABSwitch' );
     var Text = require( 'SCENERY/nodes/Text' );
+    var WaterTowerLegsNode = require( 'FLUID_PRESSURE_AND_FLOW/watertower/view/WaterTowerLegsNode' );
+
+    var optionWidth = 60;
+    var optionHeight = 50;
+    var inset = 10;
 
     /**
      * Constructor for the sluice controller
@@ -22,7 +30,59 @@ define( function( require ) {
     function SluiceControl( isSluiceOpenProperty, options ) {
 
       Node.call( this );
-      this.addChild( new ABSwitch( isSluiceOpenProperty, false, new Text( "Close" ), true, new Text( "Open" ) ) );
+
+      // close option
+      var tankDim = optionHeight * 0.4;
+      var closeOptionNode = new Rectangle( 0, 0, optionWidth, optionHeight, 5, 5, {stroke: 'gray', lineWidth: 1, fill: 'white'} );
+      var closeWaterTowerLegs = new WaterTowerLegsNode( tankDim, tankDim, {
+        legWidth: 1,
+        crossbeamWidth: 1,
+        bottom: closeOptionNode.bottom,
+        left: closeOptionNode.centerX - tankDim / 2
+      } );
+
+      var closeWaterTank = new Rectangle( 0, 0, tankDim, tankDim, {
+        stroke: 'gray',
+        lineWidth: 1,
+        fill: '#33D6FF',
+        bottom: closeWaterTowerLegs.top,
+        left: closeWaterTowerLegs.left
+      } );
+
+      closeOptionNode.addChild( closeWaterTank );
+      closeOptionNode.addChild( closeWaterTowerLegs );
+
+      // open option
+      var openOptionNode = new Rectangle( 0, 0, optionWidth, optionHeight, 5, 5, {stroke: 'gray', lineWidth: 1, fill: 'white'} );
+
+      var openWaterTowerLegs = new WaterTowerLegsNode( tankDim, tankDim, {
+        legWidth: 1,
+        crossbeamWidth: 1,
+        bottom: openOptionNode.bottom,
+        left: openOptionNode.centerX - tankDim / 2
+      } );
+
+      var openWaterTank = new Rectangle( 0, 0, tankDim, tankDim, {
+        stroke: 'gray',
+        lineWidth: 1,
+        fill: '#33D6FF',
+        bottom: openWaterTowerLegs.top,
+        left: openWaterTowerLegs.left
+      } );
+
+      var waterFlow = new Path( new Shape().moveTo( 0, 0 ).quadraticCurveTo( 12, 5, 14, 25 ), {
+        left: openWaterTank.right - 3,
+        lineWidth: 6,
+        top: openWaterTank.bottom - 8,
+        stroke: '#33D6FF'
+      } );
+      openOptionNode.addChild( openWaterTank );
+      openOptionNode.addChild( openWaterTowerLegs );
+      openOptionNode.addChild( waterFlow );
+
+      var sluicePanel = new Rectangle( 0, 0, 2 * optionWidth + 100, optionHeight + 2 * inset, 10, 10, {stroke: 'gray', lineWidth: 1, fill: 'blue'} );
+      sluicePanel.addChild( new ABSwitch( isSluiceOpenProperty, false, closeOptionNode, true, openOptionNode, {left: sluicePanel.left + inset, top: sluicePanel.top + inset} ) );
+      this.addChild( sluicePanel );
 
       this.mutate( options );
     }
