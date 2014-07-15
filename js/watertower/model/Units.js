2ab431b83a5a418a81ec9f355d55555f650325b9
Chris Malley <cmalley@pixelzoom.com>
2014-07-15 09:34:54 -0600

    add blank lines

--- js/watertower/model/Units.js
+++ js/watertower/model/Units.js
@@ -20,12 +20,15 @@ define( function( require ) {
   var PSI_PER_PASCAL = 145.04E-6;
 
   return {
+
     FLUID_DENSITY_ENGLISH_PER_METRIC: 62.4 / 1000.0,
+
     feetToMeters: function( feet ) {
       return feet / FEET_PER_METER;
     },
 
     getPressureString: {
+
       metric: function( pressure ) {
         if ( pressure === '' ) {
           return '-';
@@ -34,6 +37,7 @@ define( function( require ) {
           return Util.toFixed( pressure / 1000, 3 ) + ' ' + kPaString;
         }
       },
+
       english: function( pressure ) {
         if ( pressure === '' ) {
           return '-';
