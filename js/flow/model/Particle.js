75eaa175c5a21f15082b4910be43fcf171d01980
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-08-21 20:21:30 +0530

    Particle model ported from Java.

--- js/flow/model/Particle.js
+++ js/flow/model/Particle.js
@@ -5,67 +5,64 @@
  * @author Siddhartha Chinthapally (Actual Concepts)
  */
 
-define( function( require ) {
+define(function (require) {
   'use strict';
 
   // modules
-  var PropertySet = require( 'AXON/PropertySet' );
-  var Util = require( 'DOT/Util' );
-  var inherit = require( 'PHET_CORE/inherit' );
+  var PropertySet = require('AXON/PropertySet');
+  var Util = require('DOT/Util');
+  var inherit = require('PHET_CORE/inherit');
 
   // constants
-  var Constants = require( 'FLUID_PRESSURE_AND_FLOW/flow/Constants' );
+  var Constants = require('FLUID_PRESSURE_AND_FLOW/flow/Constants');
 
   /**
+   *
    * @param {Vector2} position of the particle
-   * @param {Vector2} velocity of the particle
-   * @param {Number} volume of the particle in m3
+   * @param {Number} fractionUpPipe
+   * @param {Pipe} container holding the particle
+   * @param {Number} radius of the particle
+   * @param {string} color of the particle
+   * @param {boolean} gridParticle whether a grid particle or a regular particle
    * @constructor
    */
-  function Particle( position, velocity, volume ) {
+  function Particle(position, fractionUpPipe, container, radius, color, gridParticle) {
 
-    PropertySet.call( this, {
-      position: position,
-      velocity: velocity,
-      volume: volume
-    } );
+    PropertySet.call(this, {
+      position: position
 
-    // node holds a reference to the particle node. This is used to remove the view element when the particle is no longer necessary.
-    // This is a shortcut to prevent having removal listeners for individual particles.
-    this.node = null;
-
-    this.addDerivedProperty( 'radius', ['volume'], function( volume ) {
-      return Util.cubeRoot( (3 * volume) / (4 * Math.PI) );
-    } );
+    });
+    this.fractionUpPipe = fractionUpPipe;
+    this.container = container;
+    this.radius = radius;
+    this.color = color;
+    this.gridPracticle = gridParticle;
   }
 
-  return inherit( PropertySet, Particle, {
+  return inherit(PropertySet, Particle, {
+
     step: function( dt ) {
-
-      //Math is done component-wise to avoid too many allocations, see https://github.com/phetsims/fluid-pressure-and-flow/issues/46
-      // v_f = v_i + a * dt
-      var accelerationY = -Constants.EARTH_GRAVITY;
-
-      var initialVelocityX = this.velocity.x;
-      var initialVelocityY = this.velocity.y;
-
-      this.velocity.setY( this.velocity.y + accelerationY * dt );
-      this.velocityProperty._notifyObservers();
-
-      // d = (v_f + v_i) * dt/2; assuming constant acceleration
-      var displacementX = (this.velocity.x + initialVelocityX) * dt / 2;
-      var displacementY = (this.velocity.y + initialVelocityY) * dt / 2;
-
-      this.position.setXY( this.position.x + displacementX, this.position.y + displacementY );
-      this.positionProperty._notifyObservers();
     },
 
-    /**
-     * Checks if the given point is within this particle.
-     * @param {Vector2} point
-     */
-    contains: function( point ) {
-      return point.distanceXY( this.position.x, this.position.y ) < this.radius;
+    getX: function () {
+      return this.position.x;
+    },
+
+    getY: function () {
+      // return this.container.fractionToLocation(this.getX(), this.fractionUpPipe);
+      return 0.5;
+    },
+
+    setX: function (x) {
+      this.position.x = x;
+    },
+
+    getPosition: function () {
+      return this.position;
+    },
+
+    getRadius: function () {
+      return this.radius;
     }
-  } );
-} );
+  });
+});
