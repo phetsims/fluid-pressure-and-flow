ebc4ce20a5db7ab24f88f343d8decd7b2c4f3388
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-09-24 18:04:11 +0530

    Moved Barometer and Sensor files from FPAP to under-pressure/common

---
+++ js/common/model/Sensor.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2002 - 2014. University of Colorado Boulder
+
+/**
+ * Model for a generic sensor with position and value.
+ * @author Siddhartha Chinthapally (Actual Concepts)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var PropertySet = require( 'AXON/PropertySet' );
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  /**
+   * @param {Vector2} position of the sensor
+   * @param {*} value as measured by the sensor
+   * @constructor
+   */
+  function Sensor( position, value ) {
+    PropertySet.call( this, {
+      position: position,
+      value: value
+    } );
+  }
+
+  return inherit( PropertySet, Sensor );
+} );
