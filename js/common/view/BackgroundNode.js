ea912e1eae68fc278a1277b339291050f3b24618
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-09-25 19:53:49 +0530

    Use sky and ground nodes instead of rectangles.

--- js/common/view/BackgroundNode.js
+++ js/common/view/BackgroundNode.js
@@ -1,9 +1,9 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 /**
- * top background Node
- * contains atmosphere and earth
+ * View for the background node containing sky and ground with an option to turned off the atmosphere
  * @author Vasily Shakhov (Mlearner)
+ * @author Siddhartha Chinthapally (Actual Concepts)
  */
 define( function( require ) {
   'use strict';
@@ -11,31 +11,30 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var LinearGradient = require( 'SCENERY/util/LinearGradient' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var SkyNode = require( 'SCENERY_PHET/SkyNode' );
+  var GroundNode = require( 'SCENERY_PHET/GroundNode' );
 
-  function BackgroundNode( model, modelViewTransform ) {
+  /**
+   * @param {UnderPressureModel} underPressureModel of the sim
+   * @param {ModelViewTransform2} modelViewTransform for transforming between model and view co-ordinates
+   * @constructor
+   */
+  function BackgroundNode( underPressureModel, modelViewTransform ) {
     Node.call( this );
 
-    //sky
-    var sky = new Rectangle( -2000, -1000, 5000, 1000 + modelViewTransform.modelToViewY( model.skyGroundBoundY ) );
+    var skyNode = new SkyNode( -20000, -5000, 50000, 5000 + modelViewTransform.modelToViewY( underPressureModel.skyGroundBoundY ), modelViewTransform.modelToViewY( underPressureModel.skyGroundBoundY ) );
+    var skyNodeWithNoAtmosphere = new Rectangle( -2000, -1000, 5000, 1000 + modelViewTransform.modelToViewY( underPressureModel.skyGroundBoundY ), { fill: 'black'} );
 
-    this.addChild( sky );
+    this.addChild( skyNode );
+    this.addChild( skyNodeWithNoAtmosphere );
 
-    model.isAtmosphereProperty.link( function( isAtmosphere ) {
-      if ( isAtmosphere ) {
-        sky.fill = new LinearGradient( 0, 0, 0, modelViewTransform.modelToViewY( model.skyGroundBoundY ) )
-          .addColorStop( 0, '#c4e6f5' )
-          .addColorStop( 1, '#daedfa' );
-      }
-      else {
-        sky.fill = 'black';
-      }
+    underPressureModel.isAtmosphereProperty.link( function( isAtmosphere ) {
+        skyNode.visible = isAtmosphere;
+        skyNodeWithNoAtmosphere.visible = !isAtmosphere;
     } );
 
-    //earth
-    this.addChild( new Rectangle( -2000, modelViewTransform.modelToViewY( model.skyGroundBoundY ), 5000, model.height - modelViewTransform.modelToViewY( model.skyGroundBoundY ) + 1000, {
-      fill: '#93774c'
-    } ) );
-
+    //Ground node
+    this.addChild( new GroundNode( -2000, modelViewTransform.modelToViewY( underPressureModel.skyGroundBoundY ), 5000, underPressureModel.height - modelViewTransform.modelToViewY( underPressureModel.skyGroundBoundY ) + 1000, 295, {topColor: '#93774C', bottomColor: '#93774C'} ) );
   }
 
   return inherit( Node, BackgroundNode, {} );
