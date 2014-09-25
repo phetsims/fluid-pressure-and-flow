b9526e5f69aeb1fec0e83ad93c09e796f2a265ff
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-09-25 21:41:09 +0530

    Code refactor

--- js/common/view/GridLinesNode.js
+++ js/common/view/GridLinesNode.js
@@ -1,8 +1,9 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 /**
- * Draw grid lines in rect (x1, y1, x2 , y2)
+ * View for grid lines
  * @author Anton Ulyanov, Vasily Shakhov (Mlearner)
+ * @author Siddhartha Chinthapally (Actual Concepts)
  */
 define( function( require ) {
   'use strict';
@@ -13,13 +14,26 @@ define( function( require ) {
   var Line = require( 'SCENERY/nodes/Line' );
   var Units = require( 'UNDER_PRESSURE/common/model/Units' );
 
-  function GridLinesNode( model, modelViewTransform, x1, y1, x2, y2, options ) {
-    Node.call( this );
+  /**
+   * Draw grid lines in rect (x1, y1, x2 , y2)
+   * @param { Property<string> }  measureUnitsProperty takes one of 'english'/'metric'/'atmospheres'
+   * @param {ModelViewTransform2} modelViewTransform to transform between model and view coordinate frames
+   * @param {Number} x1
+   * @param {Number} y1
+   * @param {Number} x2
+   * @param {Number} y2
+   * @param {Object} options to pass to the underlying node
+   * @constructor
+   */
 
+  function GridLinesNode( measureUnitsProperty, modelViewTransform, x1, y1, x2, y2, options ) {
+
+    Node.call( this );
     options = _.extend( {
       metersStep: 1,
       feetStep: 1
     }, options );
+
 
     var addLine = function( node, y ) {
       node.addChild( new Line( modelViewTransform.modelToViewX( x1 ), y, modelViewTransform.modelToViewX( x2 ), y, {stroke: 'RGB(192, 192, 192)', lineWidth: 1.5} ) );
@@ -38,11 +52,12 @@ define( function( require ) {
     this.addChild( feetGrid );
     this.addChild( metersGrid );
 
-    model.measureUnitsProperty.link( function( value ) {
-      var metersVisible = (value !== 'english');
-      metersGrid.visible = metersVisible;
-      feetGrid.visible = !metersVisible;
+    measureUnitsProperty.link( function( value ) {
+      metersGrid.visible = (value !== 'english');
+      feetGrid.visible = (value === 'english');
     } );
+
+    this.mutate( options );
   }
 
   return inherit( Node, GridLinesNode );
