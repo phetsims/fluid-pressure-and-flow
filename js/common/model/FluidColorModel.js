8ce0fab64260fa129079da4a99a9ab829bc75c52
Sam Reid <reids@colorado.edu>
2014-10-20 10:56:56 -0600

    Minor cleanup for FluidColorModel.js, see #88

--- js/common/model/FluidColorModel.js
+++ js/common/model/FluidColorModel.js
@@ -1,11 +1,11 @@
 // Copyright (c) 2002 - 2014. University of Colorado Boulder
 
 /**
- * Change fluid color when fluid density changes. For a given density the fluid color is got by linearly interpolating the
+ * Change fluid color when fluid density changes. For a given density the fluid color is got by linearly interpolating
  * the RGB values between min (gas) and max (honey).
+ *
  * @author Vasily Shakhov (Mlearner)
  * @author Siddhartha Chinthapally (Actual Concepts)
- *
  */
 define( function( require ) {
   'use strict';
@@ -17,38 +17,33 @@ define( function( require ) {
   var LinearFunction = require( 'DOT/LinearFunction' );
   var Constants = require( 'UNDER_PRESSURE/common/Constants' );
 
+  // Color constants, from the Java version
+  var GAS_COLOR = new Color( 149, 142, 139 );
+  var WATER_COLOR = new Color( 20, 244, 255 );
+  var HONEY_COLOR = new Color( 255, 191, 0 );
+
   /**
-   * @param {Property<Number>} fluidDensityProperty
+   * @param {Property.<number>} fluidDensityProperty
    * @param {Range} fluidDensityRange
    * @constructor
    */
   function FluidColorModel( fluidDensityProperty, fluidDensityRange ) {
     var fluidColorModel = this;
 
-    //from java version
-    var GAS_COLOR = new Color( 149, 142, 139 );
-    var WATER_COLOR = new Color( 20, 244, 255 );
-    var HONEY_COLOR = new Color( 255, 191, 0 );
+    var getRedLow = new LinearFunction( fluidDensityRange.min, Constants.WATER_DENSITY, GAS_COLOR.red, WATER_COLOR.red );
+    var getGreenLow = new LinearFunction( fluidDensityRange.min, Constants.WATER_DENSITY, GAS_COLOR.green, WATER_COLOR.green );
+    var getBlueLow = new LinearFunction( fluidDensityRange.min, Constants.WATER_DENSITY, GAS_COLOR.blue, WATER_COLOR.blue );
 
-    var getRedLow = new LinearFunction( fluidDensityRange.min, Constants.WATER_DENSITY, GAS_COLOR.red,
-      WATER_COLOR.red );
-    var getGreenLow = new LinearFunction( fluidDensityRange.min, Constants.WATER_DENSITY, GAS_COLOR.green,
-      WATER_COLOR.green );
-    var getBlueLow = new LinearFunction( fluidDensityRange.min, Constants.WATER_DENSITY, GAS_COLOR.blue,
-      WATER_COLOR.blue );
-
-    var getRedHigh = new LinearFunction( Constants.WATER_DENSITY, fluidDensityRange.max, WATER_COLOR.red,
-      HONEY_COLOR.red );
-    var getGreenHigh = new LinearFunction( Constants.WATER_DENSITY, fluidDensityRange.max, WATER_COLOR.green,
-      HONEY_COLOR.green );
-    var getBlueHigh = new LinearFunction( Constants.WATER_DENSITY, fluidDensityRange.max, WATER_COLOR.blue,
-      HONEY_COLOR.blue );
+    var getRedHigh = new LinearFunction( Constants.WATER_DENSITY, fluidDensityRange.max, WATER_COLOR.red, HONEY_COLOR.red );
+    var getGreenHigh = new LinearFunction( Constants.WATER_DENSITY, fluidDensityRange.max, WATER_COLOR.green, HONEY_COLOR.green );
+    var getBlueHigh = new LinearFunction( Constants.WATER_DENSITY, fluidDensityRange.max, WATER_COLOR.blue, HONEY_COLOR.blue );
 
     PropertySet.call( this, {
       color: WATER_COLOR
     } );
 
-
+    // For low density values, vary between gasoline and water.
+    // For high density values vary between water and honey.
     fluidDensityProperty.link( function( density ) {
       if ( density < Constants.WATER_DENSITY ) {
         fluidColorModel.color = new Color( getRedLow( density ), getGreenLow( density ), getBlueLow( density ) );
