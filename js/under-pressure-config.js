ae4d5ca153e4281bb30dfc9a60bcce66a632399d
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:47 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/under-pressure-config.js
+++ js/under-pressure-config.js
@@ -1,36 +1,35 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 // RequireJS configuration file for Under Pressure.
-require.config(
-  {
-    deps: [ 'under-pressure-main' ],
+require.config( {
+  deps: [ 'under-pressure-main' ],
 
-    paths: {
+  paths: {
 
-      // third party libs
-      text: '../../sherpa/text',
+    // third party libs
+    text: '../../sherpa/text',
 
-      // plugins
-      image: '../../chipper/requirejs-plugins/image',
-      string: '../../chipper/requirejs-plugins/string',
+    // plugins
+    image: '../../chipper/requirejs-plugins/image',
+    string: '../../chipper/requirejs-plugins/string',
 
-      // PhET libs, uppercase names to identify them in require.js imports
-      AXON: '../../axon/js',
-      BRAND: '../../brand/js',
-      DOT: '../../dot/js',
-      JOIST: '../../joist/js',
-      KITE: '../../kite/js',
-      PHETCOMMON: '../../phetcommon/js',
-      PHET_CORE: '../../phet-core/js',
-      SCENERY: '../../scenery/js',
-      SCENERY_PHET: '../../scenery-phet/js',
-      SHERPA: '../../sherpa',
-      SUN: '../../sun/js',
+    // PhET libs, uppercase names to identify them in require.js imports
+    AXON: '../../axon/js',
+    BRAND: '../../brand/js',
+    DOT: '../../dot/js',
+    JOIST: '../../joist/js',
+    KITE: '../../kite/js',
+    PHETCOMMON: '../../phetcommon/js',
+    PHET_CORE: '../../phet-core/js',
+    SCENERY: '../../scenery/js',
+    SCENERY_PHET: '../../scenery-phet/js',
+    SHERPA: '../../sherpa',
+    SUN: '../../sun/js',
 
-      // this sim
-      UNDER_PRESSURE: '.'
-    },
+    // this sim
+    UNDER_PRESSURE: '.'
+  },
 
-    urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
-
-  } );
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
+} );
