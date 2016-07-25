cefdf774dd6667cc528ae45f84cf85ab6b631f09
Chris Malley <cmalley@pixelzoom.com>
2016-07-25 10:55:13 -0600

    relocate ArrowButton to sun, https://github.com/phetsims/sun/issues/85

--- js/common/view/ControlSlider.js
+++ js/common/view/ControlSlider.js
@@ -13,7 +13,7 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var ArrowButton = require( 'SCENERY_PHET/buttons/ArrowButton' );
+  var ArrowButton = require( 'SUN/buttons/ArrowButton' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var fluidPressureAndFlow = require( 'FLUID_PRESSURE_AND_FLOW/fluidPressureAndFlow' );
   var inherit = require( 'PHET_CORE/inherit' );
