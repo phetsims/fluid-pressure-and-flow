1cc283374d7459ce86a83bfc69073fcc86c14361
samreid <reids@colorado.edu>
2018-11-19 12:55:05 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/common/Constants.js
+++ js/common/Constants.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * Constants used in this Sim. All units are SI (mks) unless otherwise specified.
