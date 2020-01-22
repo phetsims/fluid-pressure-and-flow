766d04a8bb8f8f5a500f7c087a2681ac50b403f7
phet-dev <phetadmin@gmail.com>
2020-01-22 03:24:54 -0700

    update copyright dates from daily grunt work

--- js/flow/model/Particle.js
+++ js/flow/model/Particle.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * Model for a simple spherical particle (Modified from watertower's WaterDrop). The red dots and black grid dots
