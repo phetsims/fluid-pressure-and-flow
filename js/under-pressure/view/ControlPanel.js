b7d4543f0fe81c2d4fbc34f4760cb2d6c6d4c06a
phet-dev <phetadmin@gmail.com>
2022-03-05 03:22:54 -0700

    update copyright dates from daily grunt work

--- js/under-pressure/view/ControlPanel.js
+++ js/under-pressure/view/ControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * Control panel that contains various tools like ruler, grid and atmosphere controls.
