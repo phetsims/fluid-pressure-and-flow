aafd3931703a73706a7e449a7f7c52aa7d4fd08d
phet-dev <phetadmin@gmail.com>
2023-03-02 04:33:23 -0700

    update copyright dates from daily grunt work

--- js/common/view/VelocitySensorNode.js
+++ js/common/view/VelocitySensorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * View for the Velocity Sensor tool. Measures the velocity at the sensor's tip and shows it in the display box.
