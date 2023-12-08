447d8eeaab776179d8209fe5e4cd7564b2fe3c24
Marla Schulz <marlaa.schulz@gmail.com>
2023-12-08 14:22:05 -0700

    Document custom layout bounds across multiple screen views to prevent future breaks in phet-io instrumentation, https://github.com/phetsims/phet-io/issues/1939.

--- js/common/Constants.js
+++ js/common/Constants.js
@@ -11,6 +11,9 @@ import fluidPressureAndFlow from '../fluidPressureAndFlow.js';
 
 const Constants = {
 
+  // A PhET wide decision was made to not update custom layout bounds even if they do not match the
+  // default layout bounds in ScreenView. Do not change these bounds as changes could break or disturb
+  // any phet-io instrumention. https://github.com/phetsims/phet-io/issues/1939
   SCREEN_VIEW_OPTIONS: { layoutBounds: new Bounds2( 0, 0, 768, 504 ) },
 
   EARTH_GRAVITY: 9.8, // m/s^2
