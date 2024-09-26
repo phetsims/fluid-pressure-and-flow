4829c10caae26b18bb0be50fa765e2f1a426fc79
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:23 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for fluid-pressure-and-flow.
+ * ESLint configuration for fluid-pressure-and-flow.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
